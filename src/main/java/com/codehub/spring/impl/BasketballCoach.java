4553bfc406250b3c528bf2cc03347e23406e220e
guoanhu <8naouguh@gmail.com>
2023-01-17 09:55:41 +0800

    test

--- src/main/java/com/codehub/spring/impl/BasketballCoach.java
+++ src/main/java/com/codehub/spring/impl/BasketballCoach.java
@@ -1,9 +1,22 @@
 package com.codehub.spring.impl;
 
 import com.codehub.spring.Coach;
+import org.springframework.beans.factory.InitializingBean;
 
-public class BasketballCoach implements Coach {
+public class BasketballCoach implements Coach, InitializingBean {
 
+    private String prop;
+
+    public void init(){
+
+        System.out.println("init");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        System.out.println("afterPropertiesSet");
+    }
 
     public static final String PERFORM_BASKET_BALL_30_MIN_WORKOUT = "perform BasketBall 30 min workout";
 
@@ -12,4 +25,13 @@ public class BasketballCoach implements Coach {
         return PERFORM_BASKET_BALL_30_MIN_WORKOUT;
     }
 
+    public String getProp() {
+        return prop;
+    }
+
+    public void setProp(String prop) {
+
+        System.out.println("set prop:"+prop);
+        this.prop = prop;
+    }
 }
